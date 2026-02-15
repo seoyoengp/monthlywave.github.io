b39c527675265cbb2be806526f3747673029900e
seoyoengp <karugos76@gmail.com>
2026-02-15 15:56:19 +0900

    Add files via upload

--- data/articles.js
+++ data/articles.js
@@ -1,1 +1,34 @@
-
+/**
+ * 빛청모 월간지 WAVE - 기본 기사 데이터 (Notion 연동)
+ * Generated at 2/15/2026, 3:52:38 PM
+ * Source: 기사 관리
+ */
+window.MONTHLYWAVE_DEFAULT_ARTICLES = [
+  {
+    "id": "30870924-1730-804e-99d1-e0a45edc14cf",
+    "category": "uncategorized",
+    "title": "지역 소상공인 지원 정책 확대... '상생 경제' 본격화",
+    "author": "Anonymous",
+    "date": "2026-02-15",
+    "body": "",
+    "views": 0
+  },
+  {
+    "id": "30870924-1730-8053-92b6-dff9de298ff8",
+    "category": "uncategorized",
+    "title": "혼자만의 시간이 필요한 순간",
+    "author": "Anonymous",
+    "date": "2026-02-15",
+    "body": "",
+    "views": 0
+  },
+  {
+    "id": "30870924-1730-80c5-adbd-dbd7442f26cd",
+    "category": "uncategorized",
+    "title": "봄을 기다리는 사람들: 도심 속 작은 갤러리 이야기",
+    "author": "Anonymous",
+    "date": "2026-02-15",
+    "body": "",
+    "views": 0
+  }
+];
